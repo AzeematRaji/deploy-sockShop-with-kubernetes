f6e211800d66257579ad806f4a17b196718b3f08
AzeematRaji <azeematjumoke@gmail.com>
2024-08-16 02:52:53 -0400

    apply terraform CI

--- terraform/s3-backend.tf
+++ terraform/s3-backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "sockshop-terraform-state-bucket" 
     key            = "eks-cluster/terraform.tfstate"     
-    region         = "us-west-2"                   
+    region         = "us-east-2"                   
     encrypt        = true
   }
 
